55579171b501df3f6c309968acc9c4b542cda988
AdaZuniga <56266464+AdaZuniga@users.noreply.github.com>
2022-04-28 15:12:17 -0500

    Mongo relationships ONE TO FEW

---
+++ Models/user.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/relationshipDemo")
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!!!")
+    })
+    .catch(err => {
+        console.log("OH NO MONGO CONNECTION ERROR!!!!")
+        console.log(err)
+    })
+
+const userSchema = new mongoose.Schema({
+    first: String,
+    last: String,
+    addresses: [
+        {
+            //tuns off an id 
+            _id: { id: false },
+            street: String,
+            city: String,
+            state: String,
+            country: String
+        }
+    ]
+})
+
+const User = mongoose.model('User', userSchema);
+
+const makeUser = async () => {
+    const u = new User({
+        first: 'Harry',
+        last: 'Potter',
+        addresses:({
+            street: '999 Sesame St.',
+            city: 'New York',
+            state: 'NY',
+            country: 'USA'})
+    });
+    u.addresses.push({
+        street: '123 Sesame St.',
+        city: 'New York',
+        state: 'NY',
+        country: 'USA'
+    });
+    u.addresses.push({
+        street: '456 Sesame St.',
+        city: 'New York',
+        state: 'NY',
+        country: 'USA'
+    });
+    const res = await u.save()
+    console.log(res)
+};
+
+
+const addAddress = async (id) => {
+    const user = await User.findById(id);
+    user.addresses.push(
+        {
+            street: '99 3rd St.',
+            city: 'New York',
+            state: 'NY',
+            country: 'USA'
+        }
+    )
+    const res = await user.save()
+    console.log(res);
+}
+
+makeUser();
